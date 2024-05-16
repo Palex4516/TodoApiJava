dfe500565eb60f5f3df8af0a11e003963b4125bb
Alexander Norgren <38076837+Palex4516@users.noreply.github.com>
2024-05-16 17:04:19 +0200

    Update todoapi_openapi.json, pom.xml, and TodoResource.java

--- src/main/java/org/example/TodoResource.java
+++ src/main/java/org/example/TodoResource.java
@@ -1,47 +1,47 @@
 package org.example;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.example.generated.ApiException;
 import org.example.generated.models.TodoItemDTO;
 
 import java.util.List;
 
-@Path("/todo")
+@Path("/todo/items")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class TodoResource {
 
     @Inject
     TodoService todoService;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Din mamma";
-    }
-
-    @GET
-    @Path("/item")
-    @Produces(MediaType.APPLICATION_JSON)
-    public TodoItemDTO todo() {
-        return new TodoItemDTO().name("Din mamma").done(false);
-    }
-
-
-    @GET
-    @Path("/tjosan")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<TodoItemDTO> tjosan() throws ApiException {
+    public List<TodoItemDTO> getAllTodoItems() throws ApiException {
         return todoService.getTodoApi().todoItemsGetAllTodoItems();
     }
 
+
+    @GET
+    @Path("/{id}")
+    public TodoItemDTO getTodoItem(Long id) throws ApiException {
+        return todoService.getTodoApi().todoItemsGetTodoItem(id);
+    }
+
     @POST
-    @Path("/tjosan")
-    @Produces(MediaType.APPLICATION_JSON)
-    public TodoItemDTO tjosanPOST(TodoItemDTO todoItemDTO) throws ApiException {
+    public TodoItemDTO createTodoItem(TodoItemDTO todoItemDTO) throws ApiException {
         return todoService.getTodoApi().todoItemsPostTodoItem(todoItemDTO);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public void updateTodoItem(Long id, TodoItemDTO todoItemDTO) throws ApiException {
+        todoService.getTodoApi().todoItemsPutTodoItem(id, todoItemDTO);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deleteTodoItem(Long id) throws ApiException {
+        todoService.getTodoApi().todoItemsDeleteTodoItem(id);
     }
 }
